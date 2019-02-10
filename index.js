d5deb10898e2e4901427f73075e35101c2c5d9cf
BanYT <44928120+BanYT@users.noreply.github.com>
2019-02-10 10:08:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const fs = require("fs");
 const client = new Discord.Client();
 require('./util/cmdloader.js')(client);//requires the command loader
 let token = process.env.token;
-let prefix = ".";
+let prefix = "-";
 
-
+client.on('ready', async => {
+console.log("Elifeur bot is ready!" + "\n" + client.user.tag + "\n" + "Server Count: "  + client.guilds.size + "\n" + "Cached users: " + client.users.size + "\n" + "Enjoy!")
+});
 client.on('ready', () => {
         client.user.setActivity(`On ${client.guilds.size} Servers With ${client.users.size} Members`, { type: "PLAYING" });
         setTimeout(game2, 20000)
@@ -18,14 +20,40 @@ client.on('ready', () => {
     }
     
     function game2() {
-        client.user.setActivity(`To .help || To Get Full List Of My Commands`, { type: "LISTENING" });
+        client.user.setActivity(`To -help || To Get Full List Of My Commands`, { type: "LISTENING" });
         setTimeout(game3, 20000)
     }
     
     function game3() {
-       client.user.setActivity(`To ${client.commands.size} Commands`, { type: "LSTENING" });
+       client.user.setActivity(`To ${client.commands.size} Commands`, { type: "LISTENING" });
         setTimeout(game1, 20000);//these times are in ms, so 30,000 = 30 seconds
     } 
+
+client.on('message', message => {
+    let messageArray = message.content.split(" ");
+    let command = messageArray[0];
+    let args = messageArray.slice(1);
+    if(command === settings.prefix + settings.rainbowcommand) {
+        const rolez = message.mentions.roles.first() || message.guild.roles.find(r=> r.name === args [0])
+        if(!rolez) return message.channel.send(settings.messageresponse.rolenotfound).catch(err=> message.channel.send("No response"))
+        if(!message.guild.member(client.user.id).hasPermission("MANAGE_ROLES")) return message.channel.send(settings.messageresponse.missingperm).catch(err=> message.channel.send("no response"))
+        var colors = settings.rainbowrole
+        var rolestart = setInterval(function() {
+            var colorsz = colors[Math.floor(Math.random() * colors.length)];
+            rolez.setColor(colorsz)
+        }, settings.rainbowdelay); 
+            message.channel.send(settings.messageresponse.success).catch(err=> message.channel.send("No response"))
+
+    }
+    if(command === settings.prefix + settings.rainbowstop) {
+            setTimeout(function () {
+           process.exit()
+            }, 1000);
+           
+                       message.channel.send(settings.messageresponse.rainbowstop).catch(err=> message.channel.send("No response"))
+                       }
+
+
 client.on('message', message => {
 
 	
