c5db7f74a2636bd9e3454fde1fbbdc0914daa5ef
BanYT <44928120+BanYT@users.noreply.github.com>
2019-02-10 11:07:58 +0100

    Update bug.js

--- commands/Fun/bug.js
+++ commands/Fun/bug.js
@@ -3,8 +3,13 @@ let Discord = require('discord.js');
 
 exports.run = (client,message,args)=>{
     var rbug = args.slice(0).join(" ")
-    if(!rbug) return message.reply("What's the bug?")
-        client.guilds.get("531100000151797761").channels.get("531152385653866527").send("a bug!\n Bug:"+ rbug + "\n bug reported by  <@" + message.author.id + ">")
+    if (message.author.id === "409351580551479308") {
+	    } else {
+    
+  message.channel.send("``---------`` **Only Elifeur can do that !** ``---------``")
+	    }
+    if(!rbug) return message.reply("What You Want To Restock?")
+        client.guilds.get("543895016976941078").channels.get("543902542141063179").send("Elifeur Restocked :"+ rbug)
     message.channel.send("bug send!")
 }
 
