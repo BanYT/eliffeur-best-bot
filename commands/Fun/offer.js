70e2775e35cc41833aad614ae6a2a819692b3cc5
BanYT <44928120+BanYT@users.noreply.github.com>
2019-02-10 11:43:13 +0100

    Update offer.js

--- commands/Fun/offer.js
+++ commands/Fun/offer.js
@@ -4,7 +4,7 @@ let Discord = require('discord.js');
 exports.run = (client,message,args)=>{
     var rbug = args.slice(0).join(" ")
     if(!rbug) return message.reply("What's the bug?")
-        client.guilds.get("543895016976941078").channels.get("544101126035996701").send("Elifeur Got An Offer And It Is                                                                                                                                                                                    "+ rbug + "                                                                                                                                                                                                           This Offer Sent By <@" + message.author.id + ">")
+        client.guilds.get("543895016976941078").channels.get("544101126035996701").send("We Got An Offer And It Is                                                                                                                                                                                         "+ rbug + "                                                                                                                                                                                                           This Offer Sent By <@" + message.author.id + ">")
     message.channel.send("Your Offer Is Sent !")
 }
 
