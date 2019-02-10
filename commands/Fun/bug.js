232f09e7046d5a7122a97e8ee8cdcd6f0c86acd0
BanYT <44928120+BanYT@users.noreply.github.com>
2019-02-10 11:17:04 +0100

    Update bug.js

--- commands/Fun/bug.js
+++ commands/Fun/bug.js
@@ -3,11 +3,7 @@ let Discord = require('discord.js');
 
 exports.run = (client,message,args)=>{
     var rbug = args.slice(0).join(" ")
-    if (message.author.id === "409351580551479308") {
-	    } else {
-    
-  message.channel.send("``---------`` **Only Elifeur can do that !** ``---------``")
-	    }
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send("You Can't Use This Command Kiddo! Because Your Missing Administrator Permission");
     if(!rbug) return message.reply("What You Want To Restock?")
         client.guilds.get("543895016976941078").channels.get("543902542141063179").send("Elifeur Restocked :                                                                                                                                                                                                                                    "+ rbug)
     message.channel.send("Your Restock Sent!")
